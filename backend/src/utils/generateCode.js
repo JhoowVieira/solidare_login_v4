e6baa5061a8dbf4587c4efb6ff353afc9e040751
Gustavo <guh24042009@gmail.com>
2026-07-07 22:36:11 -0300

    Adiciona função para gerar código de doação com formato específico

---
+++ backend/src/utils/generateCode.js
@@ -0,0 +1,16 @@
+import crypto from 'crypto'
+
+export function gerarCodigoDoacao() {
+    const hoje = new Date();
+
+    const data =
+        hoje.getFullYear() +
+        String(hoje.getMonth() + 1).padStart(2, "0") +
+        String(hoje.getDate()).padStart(2, "0");
+
+    const aleatorio = crypto.randomBytes(3).toString("hex").toUpperCase();
+
+    return `DOA-${data}-${aleatorio}`;
+}
+
+console.log(gerarCodigoDoacao());
